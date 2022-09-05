adf8ce211125c7d258ecf7bdd1b87486fb5457bb
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-05 19:09:21 -0300

    realice la validacion de los campos agregando la clase is-valid, is-invalid ademas de utilizar expresiones regulares

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 // agregar logica principal del ecommerce
+
+
+
+
+// agregamos importamos funciones y clases de otros archivos js
+
+import {validarEmail, validarPassword} from "./helpers.js";
+
+
+// declaramos variables
+let formulario = document.getElementById("loging");
+let usuario=document.getElementById("usuario");
+let password=document.getElementById("inputPassword");
+
+// agregamos los eventos.
+usuario.addEventListener('blur', ()=>{validarEmail(usuario)});
+password.addEventListener('blur', ()=>{validarPassword(password)});
+
+
+// funciones
+
--- js/helpers.js
+++ js/helpers.js
@@ -1,1 +1,24 @@
-// agregar validaciones
+// validaciones para formularios. 
+
+export function validarEmail(input){
+    let expReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(expReg.test(input.value)){
+        input.className = 'form-control is-valid';
+        return true;
+    }else{
+        input.className = 'form-control is-invalid';
+        return false;
+    }
+}
+
+export function validarPassword(input){
+    let expReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    if(expReg.test(input.value)){
+        input.className = 'form-control is-valid';
+        return true;
+    }else{
+        input.className = 'form-control is-invalid';
+        return false;
+    }
+}
+
