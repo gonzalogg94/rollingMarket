b931ccd693e742cc6f91c094095ad1ccb0e600b3
John Doe <PerezFernandoE83@gmail.com>
2022-09-05 22:00:29 -0300

    creado class producto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-// agregar logica principal del ecommerce
+import Producto from ".classProducto.js";
+
+let listaLimieza = JSON.parse(localStorage.getItem("listaLimpiezaKey")) || [];
+let listaBebidas = JSON.parse(localStorage.getItem("listaBebidasKey")) || [];
+let listaLacteos = JSON.parse(localStorage.getItem("listaLacteosKey")) || [];
+let listaElectronica = JSON.parse(localStorage.getItem("listaElectronicaKey")) || [];
