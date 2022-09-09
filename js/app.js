5a06f5190279e9ed8370a25cb3255ccf64907b08
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-09 11:44:41 -0300

    logueo en pagina index terminado , falta hacerlo en las demas paginas

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ import {
   validarPrecio,
   validarStock,
 } from "./helpers.js";
+
+
 // import {Producto} from "./classProducto.js";
 // // declaramos variables
 // let listaLimieza = JSON.parse(localStorage.getItem("listaLimpiezaKey")) || [];
@@ -14,10 +16,19 @@ import {
 // let listaLacteos = JSON.parse(localStorage.getItem("listaLacteosKey")) || [];
 // let listaElectronica = JSON.parse(localStorage.getItem("listaElectronicaKey")) || [];
 
+// variales relacionadas al inicio de sesion
+let estadoSesion = JSON.parse(sessionStorage.getItem("sesionKey"));
+let loginNosotros=document.getElementById("loginNosotros");
+// let administradorNosotros=document.getElementsById("administradorNosotros");
+let loginError=document.getElementById("loginError");
+let administradorError=document.getElementById("administradorError");
+let loginDetalles=document.getElementById("loginDetalles");
+let administradorDetalles=document.getElementById("administradorDetalles");
+
 // variables formulario producto
-const modalProducto = new bootstrap.Modal(
-  document.getElementById("productosModal")
-);
+// const modalProducto = new bootstrap.Modal(
+//   document.getElementById("productosModal")
+// );
 let formularioProductos = document.getElementById("formularioProductos");
 let codigo = document.getElementById("codigo");
 let nombre = document.getElementById("nombre");
@@ -28,6 +39,26 @@ let precio = document.getElementById("precio");
 let stock = document.getElementById("stock");
 
 // eventos formulario producto
+
+if (estadoSesion == true) {
+  loginNosotros.className = "nav-item d-none";
+  administradorNosotros.className ="nav-item";
+  loginError.className = "nav-item d-none";
+  administradorError.className = "nav-item active";
+  loginDetalles.className = "nav-item d-none";
+  administradorDetalles.className = "nav-item active";
+} else {
+  loginNosotros.className = "nav-item";
+  administradorNosotros.className = "nav-item d-none";
+  loginError.className = "nav-item";
+  administradorError.className = "nav-item d-none";
+  loginDetalles.className = "nav-item";
+  administradorDetalles.className = "nav-item d-none";
+}
+
+
+
+
 
 formularioProductos.addEventListener("submit", crearProducto);
 
--- js/index.js
+++ js/index.js
@@ -3,35 +3,46 @@ import { validarEmail, validarPassword } from "./helpers.js";
 const modalLogin = new bootstrap.Modal(document.getElementById("modalLogin"));
 const mail = "rollingmarket@gmail.com";
 const pass = "rollingMarket2022*";
+let estadoSesion = JSON.parse(sessionStorage.getItem("sesionKey"));
 let formulario = document.getElementById("loging");
 let usuario = document.getElementById("usuario");
 let password = document.getElementById("inputPassword");
+let loginIndex = document.getElementById("loginIndex");
 
-// agregamos los eventos.
-formulario.addEventListener("submit", validarAdm);
-usuario.addEventListener("blur", () => {
-  validarEmail(usuario);
-});
-password.addEventListener("blur", () => {
-  validarPassword(password);
-});
+// validar usurio con sesion storage
+if (estadoSesion == true) {
+  loginIndex.className = "nav-item d-none";
+  administrador.className = "nav-item active";
+} else {
+  // agregamos los eventos.
+  formulario.addEventListener("submit", validarAdm);
+  usuario.addEventListener("blur", () => {
+    validarEmail(usuario);
+  });
+  password.addEventListener("blur", () => {
+    validarPassword(password);
+  });
 
-// funciones
-function validarAdm(e) {
-  e.preventDefault();
-  if (mail === usuario.value && pass === password.value) {
-    let administrador = document.getElementById("administrador");
-    administrador.className = "nav-item";
-    modalLogin.hide();
-  } else {
-    Swal.fire({
-      icon: "error",
-      title: "ERROR",
-      text: "El mail o la contraseña son incorrectos",
-    });
-    limpiarLogin();
+  // funciones
+  function validarAdm(e) {
+    e.preventDefault();
+    if (mail === usuario.value && pass === password.value) {
+      let administrador = document.getElementById("administrador");
+      administrador.className = "nav-item active";
+      loginIndex.className = "nav-item d-none";
+      sessionStorage.setItem("sesionKey", JSON.stringify(true));
+      modalLogin.hide();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "ERROR",
+        text: "El mail o la contraseña son incorrectos",
+      });
+      limpiarLogin();
+    }
   }
 }
+
 function limpiarLogin() {
   formulario.reset();
   usuario.className = "form-control";
