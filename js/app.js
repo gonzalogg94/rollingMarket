c30e785509f711a9c674aa34607ffc03f8616f98
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-10 10:54:46 -0300

    agregue funcionalidad al login de la ventana error

--- js/app.js
+++ js/app.js
@@ -8,12 +8,6 @@ import {
   validarStock,
 } from "./helpers.js";
 
-// variales relacionadas al inicio de sesion de paginas nosotros
-
-// variables formulario producto
-// const modalProducto = new bootstrap.Modal(
-//   document.getElementById("productosModal")
-// );
 let formularioProductos = document.getElementById("formularioProductos");
 let codigo = document.getElementById("codigo");
 let nombre = document.getElementById("nombre");
@@ -24,75 +18,29 @@ let precio = document.getElementById("precio");
 let stock = document.getElementById("stock");
 
 
-if (estadoSesion == true) {
-  loginNosotros.className = "d-none";
-  administradorNosotros.className ="nav-item";
-  loginError.className = "d-none";
-  administradorError.className = "nav-item";
-  loginDetalles.className = "nav-item d-none";
-  administradorDetalles.className = "nav-item active";
-} else {
-  console.log("el usurario no esta logueado")
-}
-
-formLoginNosotros.addEventListener("submit", validarUsuarioNosotros);
-
-function validarUsuarioNosotros(e) {
-  e.preventDefault();
-  if (mail === usuarioNosotros.value && pass === passwordNosotros.value) {
-    administradorNosotros.className = "nav-item";
-    loginNosotros.className = "d-none";
-    sessionStorage.setItem("sesionKey",JSON.stringify(true));
-    console.log("usuario logueado")
-    modalLoginNosotros.hide();
-  } else {
-    Swal.fire({
-      icon: "error",
-      title: "ERROR",
-      text: "El mail o la contraseña son incorrectos",
-    });
-    limpiarLogin();
-  }
-}
-function limpiarLogin() {
-  formulario.reset();
-  usuarioNosotros.className = "form-control";
-  passwordNosotros.className = "form-control";
-}
-
-
-
-
-
-
-
-
-
-
-
 // eventos formulario producto
-// codigo.addEventListener("blur", () => {
-//   validarCodigo(codigo);
-// });
-// nombre.addEventListener("blur", () => {
-//   validarNombre(nombre);
-// });
-// descripcion.addEventListener("blur", () => {
-//   validarDescripcion(descripcion);
-// });
+codigo.addEventListener("blur", () => {
+  validarCodigo(codigo);
+});
+nombre.addEventListener("blur", () => {
+  validarNombre(nombre);
+});
+descripcion.addEventListener("blur", () => {
+  validarDescripcion(descripcion);
+});
 
-// imagen.addEventListener("blur", () => {
-//   validarImagen(imagen);
-// });
-// categoria.addEventListener("blur", () => {
-//   validarCategoria(categoria);
-// });
-// precio.addEventListener("blur", () => {
-//   validarPrecio(precio);
-// });
-// stock.addEventListener("blur", () => {
-//   validarStock(stock);
-// });
+imagen.addEventListener("blur", () => {
+  validarImagen(imagen);
+});
+categoria.addEventListener("blur", () => {
+  validarCategoria(categoria);
+});
+precio.addEventListener("blur", () => {
+  validarPrecio(precio);
+});
+stock.addEventListener("blur", () => {
+  validarStock(stock);
+});
 
 
 
--- js/nosotros.js
+++ js/nosotros.js
@@ -1,21 +1,20 @@
-import { validarPassword,validarEmail } from "./helpers.js";
+import { validarPassword, validarEmail } from "./helpers.js";
 
 // Declaracion de variables.
-
-const modalLoginNosotros = new bootstrap.Modal(document.getElementById("modalLoginNosotros"));
+const modalLoginNosotros = new bootstrap.Modal(
+  document.getElementById("modalLoginNosotros")
+);
 const mail = "rollingmarket@gmail.com";
 const pass = "rollingMarket2022*";
-let formLoginNosotros=document.getElementById("formLoginNosotros");
+let formLoginNosotros = document.getElementById("formLoginNosotros");
 let estadoSesion = JSON.parse(sessionStorage.getItem("sesionKey"));
-let usuarioNosotros=document.getElementById("usuarioNosotros");
-let passwordNosotros=document.getElementById("passwordNosotros");
-let opcionAdministrador=document.getElementById("opcionAdministrador");
-let opcionLogin=document.getElementById("opcionLogin");
+let usuarioNosotros = document.getElementById("usuarioNosotros");
+let passwordNosotros = document.getElementById("passwordNosotros");
+let opcionAdministrador = document.getElementById("opcionAdministrador");
+let opcionLogin = document.getElementById("opcionLogin");
 
 // declaracion de eventos.
-
 formLoginNosotros.addEventListener("submit", validarAdm);
-
 usuarioNosotros.addEventListener("blur", () => {
   validarEmail(usuarioNosotros);
 });
@@ -24,29 +23,32 @@ passwordNosotros.addEventListener("blur", () => {
 });
 
 // Validar usuario al iniciar la pagina
-
-
 if (estadoSesion == true) {
-    opcionLogin.className="nav-item d-none"
-    opcionAdministrador.className="nav-item"
-  } else{
-    validarAdm();
-  }
+  opcionLogin.className = "nav-item d-none";
+  opcionAdministrador.className = "nav-item";
+} else {
+  validarAdm();
+}
 
 // funciones
 function validarAdm(e) {
-    e.preventDefault();
-    if (mail === usuarioNosotros.value && pass === passwordNosotros.value) {
-      opcionAdministrador.className = "nav-item active";
-      opcionLogin.className = "nav-item d-none";
-      sessionStorage.setItem("sesionKey", JSON.stringify(true));
-      modalLoginNosotros.hide();
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "ERROR",
-        text: "El mail o la contraseña son incorrectos",
-      });
-      limpiarLogin();
-    }
+  e.preventDefault();
+  if (mail === usuarioNosotros.value && pass === passwordNosotros.value) {
+    opcionAdministrador.className = "nav-item active";
+    opcionLogin.className = "nav-item d-none";
+    sessionStorage.setItem("sesionKey", JSON.stringify(true));
+    modalLoginNosotros.hide();
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "ERROR",
+      text: "El mail o la contraseña son incorrectos",
+    });
+    limpiarLogin();
   }
+}
+function limpiarLogin() {
+  formLoginNosotros.reset();
+  usuarioNosotros.className = "form-control";
+  passwordNosotros.className = "form-control";
+}
