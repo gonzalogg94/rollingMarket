87a9840bcc486d58c322df11a05f5b005f3c6e24
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-10 10:13:25 -0300

    pagina index y nosotros funcionan para poder loguearse y moverse entre si

--- js/app.js
+++ js/app.js
@@ -9,21 +9,7 @@ import {
 } from "./helpers.js";
 
 // variales relacionadas al inicio de sesion de paginas nosotros
-const modalLoginNosotros = new bootstrap.Modal(document.getElementById("modalLoginNosotros"));
-let formLoginNosotros=document.getElementById("formLoginNosotros");
-const mail = "rollingmarket@gmail.com";
-const pass = "rollingMarket2022*";
-let estadoSesionNosotros = JSON.parse(sessionStorage.getItem("sesionKey"));
-let loginNosotros=document.getElementById("loginNosotros");
-// let administradorNosotros=document.getElementsById("administradorNosotros");
-let loginError=document.getElementById("loginError");
-let administradorError=document.getElementById("administradorError");
-let loginDetalles=document.getElementById("loginDetalles");
-let administradorDetalles=document.getElementById("administradorDetalles");
-let usuarioNosotros=document.getElementById("usuarioNosotros");
-let passwordNosotros=document.getElementById("passwordNosotros");
-let administradorNosotros=document.getElementById("administradorNosotros");
-let loginPaginaNosotros=document.getElementById("loginPaginaNosotros");
+
 // variables formulario producto
 // const modalProducto = new bootstrap.Modal(
 //   document.getElementById("productosModal")
@@ -38,11 +24,11 @@ let precio = document.getElementById("precio");
 let stock = document.getElementById("stock");
 
 
-if (estadoSesionNosotros == true) {
-  loginNosotros.className = "nav-item d-none";
+if (estadoSesion == true) {
+  loginNosotros.className = "d-none";
   administradorNosotros.className ="nav-item";
-  loginError.className = "nav-item d-none";
-  administradorError.className = "nav-item active";
+  loginError.className = "d-none";
+  administradorError.className = "nav-item";
   loginDetalles.className = "nav-item d-none";
   administradorDetalles.className = "nav-item active";
 } else {
